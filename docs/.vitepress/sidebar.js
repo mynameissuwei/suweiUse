65dfd093606b2d9b499d2be31f06dd00b8213ffb
mynameissuwei <910933955@qq.com>
2022-11-23 18:00:01 +0800

    feature: init

---
+++ docs/.vitepress/sidebar.js
@@ -0,0 +1,20 @@
+export const sidebar = {
+  '/en/': [
+    {
+      text: 'Guide',
+      items: [
+        { text: 'Button', link: '/en/' },
+        { text: 'useToggle', link: '/useToggle/' },
+      ],
+    },
+  ],
+  '/': [
+    {
+      text: '介绍',
+      items: [
+        { text: '按钮', link: '/' },
+        { text: 'useToggle', link: '/useToggle/' },
+      ],
+    },
+  ],
+};
---
+++ src/useToggle/index.zh-CN.md
@@ -0,0 +1,12 @@
+---
+map:
+  path: /useToggle
+---
+
+## 按钮
+
+渲染效果如下
+
+
+
+<API src="../button.vue" lang="zh"></API>
